d9af7222af114847335b5ec022fea195f5c21589
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2023-05-23 23:25:24 +0900

    blog code

---
+++ 20_azure_computer_vision/README.md
@@ -0,0 +1,14 @@
+# OCR
+
+## 環境変数セット
+
+```text
+export ACCOUNT_KEY=your-api-key
+export END_POINT=your-end-point
+```
+
+## Python環境のセットアップ
+
+```text
+pip install azure-cognitiveservices-vision-computervision
+```
---
+++ 20_azure_computer_vision/main.py
@@ -0,0 +1,45 @@
+import os
+import time
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import ComputerVisionOcrErrorException
+
+
+key = os.environ["ACCOUNT_KEY"]
+endpoint = os.environ["END_POINT"]
+image_path = "img/PXL_20230523_135425927.MP.jpg"  # ローカルの画像パス
+
+
+def main(image_path):
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(key))
+    local_image = open(image_path, "rb")
+    try:
+        recognize_results = computervision_client.read_in_stream(local_image, language="ja", raw=True)
+    except ComputerVisionOcrErrorException as e:
+        print("errors:", e.response)
+        raise e
+    # 結果を取得するための操作IDを取得
+    operation_location_remote = recognize_results.headers["Operation-Location"]
+    operation_id = operation_location_remote.split("/")[-1]
+
+    # 結果が利用可能になるまで待つ
+    while True:
+        get_text_results = computervision_client.get_read_result(operation_id)
+        if get_text_results.status not in ["notStarted", "running"]:
+            break
+        time.sleep(1)
+
+    # テキストの出力
+    if get_text_results.status == OperationStatusCodes.succeeded:
+        with open("output.txt", "w", encoding="utf-8") as f:
+            for text_result in get_text_results.analyze_result.read_results:
+                for line in text_result.lines:
+                    f.write(line.text + "\n")
+
+
+print("Texts are written to output.txt")
+
+
+if __name__ == "__main__":
+    main(image_path)
