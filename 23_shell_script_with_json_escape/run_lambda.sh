662492bf826a42790552f5c7ff455be8a4ace37b
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2023-08-13 22:59:09 +0900

    20230813

--- 23_shell_script_with_json_escape/run_lambda.sh
+++ 23_shell_script_with_json_escape/run_lambda.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 # AWS CLIを使用してLambda関数を実行するシェルスクリプト
 
-
 function run_lambda () {
+  payload=$(jq -n --arg p1 "$1" --arg p2 "$2" '{param_1: $p1, params: {param_2: $p2}}')
   aws lambda invoke \
   --region "<lambda-region>" \
   --function-name shell-script-with-json-escape-handler \
-  --payload '{"param_1": "'"$1"'", "params": {"param_2": "'"$2"'"}}' \
-  --profile "<iam-role>" \
+  --payload "$payload" \
+  --profile "<your-iam-role>" \
   --cli-binary-format raw-in-base64-out response.json
+
   if [ $? -ne 0 ]; then
     echo "shell-script-with-json-escape-handlerの実行に失敗しました。"
+    exit 1
   fi
 }
 
 echo "**********START**********"
 
-output="ほげoutput: python Error: Expecting value: line 1 column 1 (char 0)
+output="output: python Error: Expecting value: line 1 column 1 (char 0)
 
 
 "
 
-output_with_newline=$(echo "$output" | sed 's/$/\\n/' | tr -d '\n')
-
-echo "$output_with_newline"
-
-run_lambda "AAA" "$output_with_newline"
-
+run_lambda "AAA" "$output"
 
 # 実行結果の表示
 cat response.json
