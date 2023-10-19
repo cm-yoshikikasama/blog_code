d8f768aa21c9dc1417ee937ee329099f6c7d041b
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2023-10-19 22:31:42 +0900

    26

---
+++ 26_iics_csv/memo.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS memo(
+	seq	BIGINT,
+	memo	VARCHAR(100) ,
+	update_at	TIMESTAMP
+)
+DISTSTYLE AUTO
+SORTKEY AUTO;
+
+
+-- inserted data
+INSERT INTO memo (seq,memo, update_at) VALUES
+	(1,'①テスト', '2024-01-01 00:00:00'),
+	(2,'test2\\hoge', '2024-01-01 00:00:00'),
+	(3,'test3"', '2024-01-01 00:00:00'),
+	(4,'test4,', '2024-01-01 00:00:00')
+;
