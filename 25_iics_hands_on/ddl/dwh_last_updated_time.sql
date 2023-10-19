81d495c7872816ef735ade2cd7d55a27b054adf8
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2023-10-19 22:32:20 +0900

    25

--- 25_iics_hands_on/ddl/dwh_last_updated_time.sql
+++ 25_iics_hands_on/ddl/dwh_last_updated_time.sql
@@ -4,3 +4,9 @@ CREATE TABLE
     last_updated TIMESTAMP NOT NULL,
     PRIMARY KEY (table_name)
   );
+
+  -- 初期データのUpsert 
+INSERT INTO last_updated_time (table_name, last_updated) 
+VALUES 
+    ('event', '2000-01-01 00:00:00'),
+    ('sales', '2000-01-01 00:00:00');
