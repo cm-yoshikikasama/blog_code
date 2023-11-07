7b59d0db366eb72679c7195fad66cfe936ef890f
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2023-11-08 05:45:56 +0900

    blog 27

---
+++ 27_gpt_4_vision_preview/file_image.py
@@ -0,0 +1,41 @@
+import os
+import base64
+import requests
+
+# OpenAI API Key
+api_key = os.environ["OPENAI_API_KEY"]
+
+
+# Function to encode the image
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+# 読み取りたい画像は実行するpythonファイルと同じ階層にimgフォルダを格納し配置。
+image_path = "./img/Screenshot 2023-11-07 at 7.09.18.png"
+
+# Getting the base64 string
+base64_image = encode_image(image_path)
+
+headers = {"Content-Type": "application/json", "Authorization": f"Bearer {api_key}"}
+
+payload = {
+    "model": "gpt-4-vision-preview",
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Output the details of the objects in the image as JSON data. The resulting JSON file should start with 'start--' and end with '--end'.",
+                },
+                {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}},
+            ],
+        }
+    ],
+    "max_tokens": 300,
+}
+response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+print(response.json())
