b4d3e3887c9118a04fdf99c16a1db41f3bcaf035
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-04-29 12:53:03 +0900

    first commit 35

---
+++ 35_step_functions_glue_python_shell_with_cdk/cdk/test/app.test.ts
@@ -0,0 +1,23 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { ETLStack } from "../lib/stack/etl-stack";
+import { devParameter } from "../parameter";
+
+test("Snapshot test for ServerlessApi Stack", () => {
+  const app = new cdk.App();
+  const stack = new ETLStack(app, "ETL", {
+    description: "S3CopyFlow (tag:kasama-test-tag)",
+    env: {
+      account: devParameter.env?.account || process.env.CDK_DEFAULT_ACCOUNT,
+      region: devParameter.env?.region || process.env.CDK_DEFAULT_REGION,
+    },
+    tags: {
+      Repository: "kasama-test-tag",
+      Environment: devParameter.envName,
+    },
+
+    projectName: devParameter.projectName,
+    envName: devParameter.envName,
+  });
+  expect(Template.fromStack(stack)).toMatchSnapshot();
+});
