383adadcea887b9872abba4ce1474320e97e3261
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-06-29 10:14:04 +0900

    blog39

---
+++ 38_eventbridge_alert_delete_with_cdk/cdk/lib/stack/etl-stack.ts
@@ -0,0 +1,56 @@
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import { S3Construct } from "../constructs/s3";
+import { LambdaConstruct, LambdaConstructProps } from "../constructs/lambda";
+import {
+  StepFunctionsConstruct,
+  StepFunctionsConstructProps,
+} from "../constructs/step-functions";
+import {
+  EventBridgeConstruct,
+  EventBridgeConstructProps,
+} from "../constructs/eventbridge";
+
+export interface ETLStackProps extends cdk.StackProps {
+  envName: string;
+  projectName: string;
+}
+
+export class ETLStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ETLStackProps) {
+    super(scope, id, props);
+    // スタックレベルで警告を抑制
+    this.node.addMetadata(
+      "aws:cdk:warning:@aws-cdk/aws-events:ruleUnresolvedEnvironment",
+      false
+    );
+    const s3Construct = new S3Construct(this, "S3", {
+      envName: props.envName,
+      projectName: props.projectName,
+    });
+    const lambdaConstruct = new LambdaConstruct(this, "Lambda", {
+      envName: props.envName,
+      projectName: props.projectName,
+      dataSourceBucketName: s3Construct.dataSourceBucket.bucketName,
+      dataStoreBucketName: s3Construct.dataStoreBucket.bucketName,
+    } as LambdaConstructProps);
+
+    const stepFunctionsConstruct = new StepFunctionsConstruct(
+      this,
+      "StepFunctions",
+      {
+        envName: props.envName,
+        projectName: props.projectName,
+        dataSourceBucketName: s3Construct.dataSourceBucket.bucketName,
+        dataStoreBucketName: s3Construct.dataStoreBucket.bucketName,
+        lambdaName: lambdaConstruct.LambdaName,
+      } as StepFunctionsConstructProps
+    );
+    new EventBridgeConstruct(this, "EventBridge", {
+      envName: props.envName,
+      projectName: props.projectName,
+      dataSourceBucketName: s3Construct.dataSourceBucket.bucketName,
+      stateMachineArn: stepFunctionsConstruct.stateMachine.stateMachineArn,
+    } as EventBridgeConstructProps);
+  }
+}
---
+++ 39_eventbridge_no_empty_and_folder_with_cdk/cdk/lib/constructs/lambda.ts
@@ -0,0 +1,36 @@
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export interface LambdaConstructProps {
+  envName: string;
+  projectName: string;
+}
+
+export class LambdaConstruct extends Construct {
+  public readonly lambdaArn: string;
+  constructor(scope: Construct, id: string, props: LambdaConstructProps) {
+    super(scope, id);
+    const lambdaRole = new iam.Role(this, "LambdaExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSLambdaBasicExecutionRole"
+        ),
+      ],
+    });
+    const lambdaName = `${props.projectName}-${props.envName}-event-trigger-test-handler`;
+    const lambdaFunction = new lambda.Function(this, "EtlHandler", {
+      functionName: lambdaName,
+      runtime: lambda.Runtime.PYTHON_3_12,
+      code: lambda.Code.fromAsset("../resources/"),
+      handler: "handler.main",
+      memorySize: 512,
+      timeout: cdk.Duration.seconds(900),
+      role: lambdaRole,
+      architecture: lambda.Architecture.ARM_64,
+    });
+    this.lambdaArn = lambdaFunction.functionArn;
+  }
+}
