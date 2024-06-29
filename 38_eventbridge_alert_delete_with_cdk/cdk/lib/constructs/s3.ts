bfaa198bdbe1c96f1bc0478de8cd63abc5b6c3a5
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-06-29 10:14:04 +0900

    blog39

---
+++ 38_eventbridge_alert_delete_with_cdk/cdk/lib/constructs/s3.ts
@@ -0,0 +1,36 @@
+import * as cdk from "aws-cdk-lib";
+import {
+  Bucket,
+  BlockPublicAccess,
+  BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+
+export interface S3ConstructProps {
+  envName: string;
+  projectName: string;
+}
+
+export class S3Construct extends Construct {
+  public readonly dataSourceBucket: Bucket;
+  public readonly dataStoreBucket: Bucket;
+  constructor(scope: Construct, id: string, props: S3ConstructProps) {
+    super(scope, id);
+
+    this.dataSourceBucket = new Bucket(this, "DataSourceBucket", {
+      bucketName: `${props.projectName}-${props.envName}-data-source`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.KMS_MANAGED,
+      versioned: true,
+      eventBridgeEnabled: true,
+    });
+    this.dataStoreBucket = new Bucket(this, "DataStoreBucket", {
+      bucketName: `${props.projectName}-${props.envName}-data-store`,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      encryption: BucketEncryption.KMS_MANAGED,
+      versioned: true,
+    });
+  }
+}
---
+++ 38_eventbridge_alert_delete_with_cdk/resources/lambda/etl_handler.py
@@ -0,0 +1,53 @@
+import os
+import boto3
+import traceback
+import pandas as pd
+from io import StringIO
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from lib.get_logger import GetLogger
+
+get_logger = GetLogger(__name__)
+logger = get_logger.logger
+
+
+def main(event, context):
+    try:
+        # ジョブから渡されたパラメータを使用
+        s3_input_bucket = event["detail"]["bucket"]["name"]
+        s3_input_key = event["detail"]["object"]["key"]
+        s3_output_bucket = os.environ["S3_OUTPUT_BUCKET"]
+        s3_output_key = os.environ["S3_OUTPUT_KEY_PREFIX"]
+        logger.info(f"s3_input_bucket:{s3_input_bucket}")
+        logger.info(f"s3_input_key:{s3_input_key}")
+        # Boto3クライアントの初期化
+        s3_client = boto3.client("s3")
+
+        # S3からCSVファイルを読み込む
+        response = s3_client.get_object(Bucket=s3_input_bucket, Key=s3_input_key)
+        input_data = pd.read_csv(response["Body"])
+
+        # 動物園に来てからの年数を計算して新しい列に追加
+        current_year = datetime.now(ZoneInfo("Asia/Tokyo")).year
+        input_data["years_in_zoo"] = current_year - input_data["arrival_year"]
+
+        # Pandas DataFrameをCSV文字列に変換
+        output_csv = input_data.to_csv(index=False)
+        output_file = StringIO(output_csv)
+
+        current_time = datetime.now(ZoneInfo("Asia/Tokyo")).strftime("%Y-%m-%d-%H-%M-%S")
+        # 現在の日時を取得し、ファイル名を生成
+        output_filename = f"{s3_output_key}output_{current_time}.csv"
+
+        # 加工後のCSVをS3に保存
+        s3_client.put_object(
+            Bucket=s3_output_bucket,
+            Key=output_filename,
+            Body=output_file.getvalue(),
+        )
+        logger.info(f"Successful put {s3_output_bucket}/{output_filename}")
+        return {"statusCode": 200, "body": f"Successful put {s3_output_bucket}/{output_filename}"}
+    except Exception as e:
+        tb = traceback.format_exc()
+        logger.error(f"Unexpected error: {str(e)}\nTraceback: {tb}")
+        raise e
