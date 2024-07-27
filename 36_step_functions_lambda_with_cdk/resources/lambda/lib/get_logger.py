97878542743a939a72ad0b76ab4c03376dc46306
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-07-27 10:33:44 +0900

    fix logging

--- 36_step_functions_lambda_with_cdk/resources/lambda/lib/get_logger.py
+++ 36_step_functions_lambda_with_cdk/resources/lambda/lib/get_logger.py
@@ -1,23 +1,17 @@
-import sys
+import os
+import boto3
 import logging
 
 
 class GetLogger:
     def __init__(self, name):
+
+        log_level = "DEBUG"
+        print("log_level:", log_level)
         self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(name)s - %(funcName)s  - line:%(lineno)d - %(message)s"
-        )
-
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(formatter)
-
-        warning_handler = logging.StreamHandler(sys.stderr)
-        warning_handler.setLevel(logging.WARNING)
-        warning_handler.setFormatter(formatter)
-
-        self.logger.addHandler(console_handler)
-        self.logger.addHandler(warning_handler)
+        self.logger.setLevel(log_level)
+        self.logger.propagate = False  # propagateを設定
+        formatter = logging.Formatter("%(levelname)s - %(name)s - %(funcName)s - line:%(lineno)d - %(message)s")
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
