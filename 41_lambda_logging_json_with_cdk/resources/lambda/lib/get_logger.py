9055f0c72e23c46fc21881e1d294fb0903cc3943
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-08-20 12:57:10 +0900

    blog 41

---
+++ 41_lambda_logging_json_with_cdk/resources/lambda/lib/get_logger.py
@@ -0,0 +1,37 @@
+import logging
+import json
+import traceback
+
+
+class CustomJsonFormatter(logging.Formatter):
+    def format(self, record):
+        log_entry = {
+            "timestamp": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "logger": record.name,
+            "requestId": getattr(record, "aws_request_id", None),
+            "funcName": record.funcName,
+            "lineno": record.lineno,
+        }
+        if record.exc_info:
+            log_entry.update(
+                {
+                    "stackTrace": traceback.format_exception(*record.exc_info),
+                    "errorType": record.exc_info[0].__name__,
+                    "errorMessage": str(record.exc_info[1]),
+                    "location": f"{record.pathname}:{record.funcName}:{record.lineno}",
+                }
+            )
+        return json.dumps(log_entry)
+
+
+def get_logger():
+    logger = logging.getLogger()
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = CustomJsonFormatter("%(asctime)s")
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
