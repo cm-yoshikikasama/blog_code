1487811036ed7c614401073eeee9d7d568b2111d
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2024-12-27 17:28:13 +0900

    45

---
+++ 45_athea_partition/ddl/cm_kasama_db.test_table.sql
@@ -0,0 +1,24 @@
+CREATE EXTERNAL TABLE cm_kasama_db.test_table(
+  id STRING,
+  name STRING
+  )
+PARTITIONED BY (
+    year string,
+    month string,
+    day string
+)
+STORED AS PARQUET
+LOCATION 's3://cm-kasama-sf-test/test_table/'
+TBLPROPERTIES (
+    'projection.enabled' = 'true',
+    'projection.year.type' = 'date',
+    'projection.year.format' = 'yyyy',
+    'projection.year.range' = '2024,NOW',
+    'projection.month.type' = 'integer',
+    'projection.month.range' = '1,12',
+    'projection.month.digits' = '2',
+    'projection.day.type' = 'integer',
+    'projection.day.range' = '1,31',
+    'projection.day.digits' = '2',
+    'storage.location.template' = 's3://cm-kasama-sf-test/test_table/${year}/${month}/${day}'
+);
