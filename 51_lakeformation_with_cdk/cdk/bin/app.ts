370b1ea167cf7b97cb303177706b1002c6cb34c3
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2025-03-12 20:55:05 +0900

    51_lakeformation_with_cdk

---
+++ 51_lakeformation_with_cdk/cdk/bin/app.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { LakeFormationHybridStack } from "../lib/lakeformation";
+
+const app = new cdk.App();
+new LakeFormationHybridStack(app, "CMKasamaLakeFormation", {
+	description: "ETL (tag:kasama-test-tag)",
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
+	tags: {
+		Repository: "kasama-test-tag",
+	},
+
+	projectName: "cm-kasama",
+	envName: "dev",
+});
