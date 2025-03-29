f6c26cc89320daac8813d58018d6c514a706de08
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2025-03-30 07:41:16 +0900

    5

---
+++ 53_duckdb_vs_awswrangler/cdk/bin/app.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { LambdaStack } from "../lib/lambda-stack";
+
+const app = new cdk.App();
+
+new LambdaStack(app, `CMKasamaLambdaCsvToParquet`, {
+  description: `CMKasamaLambdaCsvToParquet`,
+  env: {
+    account:  process.env.CDK_DEFAULT_ACCOUNT,
+    region:  process.env.CDK_DEFAULT_REGION,
+  },
+  tags: {
+    Repository: `CMKasamaLambdaCsvToParquet-test-tag`,
+  },
+
+  projectName: "cm-kasama",
+  envName: "dev",
+});
