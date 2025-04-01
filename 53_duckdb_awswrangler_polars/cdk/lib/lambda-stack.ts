beb650a8d5c4aae9fc9195a1bee4569ee4e6c0ee
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2025-04-02 07:23:15 +0900

    fix 53

--- 53_duckdb_awswrangler_polars/cdk/lib/lambda-stack.ts
+++ 53_duckdb_awswrangler_polars/cdk/lib/lambda-stack.ts
@@ -2,6 +2,9 @@ import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as gluel2 from "@aws-cdk/aws-glue-alpha";
 import * as path from "path";
 import { PythonLayerVersion } from "@aws-cdk/aws-lambda-python-alpha";
 
@@ -118,6 +121,70 @@ export class LambdaStack extends cdk.Stack {
 				destination_key: destination_key,
 			},
 			architecture: lambda.Architecture.ARM_64,
+		});
+
+		// ===== Glueでサンプルデータ作成する設定 =====
+
+		// ===== 共通のS3バケット設定 =====
+		const scriptBucket = new s3.Bucket(this, "ScriptBucket", {
+			bucketName: `${props.projectName}-${props.envName}-scripts-${this.account}-${this.region}`,
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+		});
+
+		// スクリプトをS3にアップロード
+		new s3deploy.BucketDeployment(this, "DeployScript", {
+			sources: [
+				s3deploy.Source.asset(path.join(__dirname, "../../resources/glue")),
+			],
+			destinationBucket: scriptBucket,
+			destinationKeyPrefix: "scripts",
+		});
+
+		// L2用のIAMロール作成
+		const glueJobRoleL2 = new iam.Role(this, "GlueJobRoleL2", {
+			roleName: `${props.projectName}-${props.envName}-etl-glue-l2-execution-role`,
+			assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+			managedPolicies: [
+				iam.ManagedPolicy.fromAwsManagedPolicyName(
+					"service-role/AWSGlueServiceRole",
+				),
+				iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+			],
+		});
+		// CloudWatchLogsへのアクセス権限を明示的に設定
+		glueJobRoleL2.addToPolicy(
+			new iam.PolicyStatement({
+				resources: ["arn:aws:logs:*:*:*:/aws-glue/*"],
+				actions: [
+					"logs:CreateLogGroup",
+					"logs:CreateLogStream",
+					"logs:PutLogEvents",
+				],
+			}),
+		);
+
+		new gluel2.PythonShellJob(this, "SamplePythonShellJobL2", {
+			jobName: `${props.projectName}-${props.envName}-sample-job-l2`,
+			role: glueJobRoleL2,
+			description: "Sample Python Shell Job (L2)",
+			glueVersion: gluel2.GlueVersion.V3_0,
+			pythonVersion: gluel2.PythonVersion.THREE_NINE,
+			script: gluel2.Code.fromBucket(
+				scriptBucket,
+				"scripts/create_duckdb_csv.py",
+			),
+			maxCapacity: gluel2.MaxCapacity.DPU_1,
+			defaultArguments: {
+				"--env": props.envName,
+				"--project": props.projectName,
+				"--file_bucket": source_bucket,
+				"--file_key": "src/bigdata_sample.csv",
+				"--total_raws": "1000000",
+				"--additional-python-modules": "duckdb",
+			},
+			timeout: cdk.Duration.minutes(30),
+			continuousLogging: { enabled: true },
 		});
 	}
 }
