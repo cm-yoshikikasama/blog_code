90015d3f083fee4df3cae6d6d6146016fb99af52
cm-yoshikikasama <kasama.yoshiki@classmethod.jp>
2025-05-04 09:09:59 +0900

    54

---
+++ 54_dms_serverless/cdk/bin/app.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { DmsServerlessStack } from "../lib/dms-serverless-stack";
+
+const app = new cdk.App();
+
+new DmsServerlessStack(app, `CmKasamaDmsServerlessStack`, {
+	description: `CmKasamaDmsServerlessStack`,
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION,
+	},
+	tags: {
+		Repository: `CmKasamaDmsServerlessStack-test-tag`,
+	},
+
+	projectName: "cm-kasama",
+	envName: "dev",
+});
