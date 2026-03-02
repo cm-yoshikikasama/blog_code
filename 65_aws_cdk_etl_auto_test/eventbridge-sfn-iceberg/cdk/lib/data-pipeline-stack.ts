c68b7dbdb3c1d6d68c5f5e623fe9851ab63f8269
kasama.yoshiki <kasama.yoshiki@classmethod.jp>
2026-03-02 19:46:10 +0900

    rewrite PreToolUse hook from Bash to Python

--- 65_aws_cdk_etl_auto_test/eventbridge-sfn-iceberg/cdk/lib/data-pipeline-stack.ts
+++ 65_aws_cdk_etl_auto_test/eventbridge-sfn-iceberg/cdk/lib/data-pipeline-stack.ts
@@ -151,7 +151,7 @@ export class DataPipelineStack extends cdk.Stack {
 
     // Step Functions
     const waitBeforeProcess = new sfn.Wait(this, 'WaitBeforeProcess', {
-      time: sfn.WaitTime.duration(cdk.Duration.minutes(5)),
+      time: sfn.WaitTime.duration(cdk.Duration.minutes(1)),
       comment: 'Simulate a long-running pipeline for polling/resume testing',
     });
 
